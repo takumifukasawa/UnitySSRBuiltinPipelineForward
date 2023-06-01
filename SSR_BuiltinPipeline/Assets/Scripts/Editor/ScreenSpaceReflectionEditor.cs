b6a18377cec92846aba2e5a65f9be5bae3d44641
takumifukasawa <takumifukasawa.com>
2023-06-01 20:13:49 +0900

    remove unused codes

--- SSR_BuiltinPipeline/Assets/Scripts/Editor/ScreenSpaceReflectionEditor.cs
+++ SSR_BuiltinPipeline/Assets/Scripts/Editor/ScreenSpaceReflectionEditor.cs
@@ -9,14 +9,6 @@ public sealed class ScreenSpaceReflectionEditor : PostProcessEffectEditor<Screen
     SerializedParameterOverride m_ReflectionAdditionalRate;
     SerializedParameterOverride m_ReflectionRayThickness;
 
-    SerializedParameterOverride m_OcclusionSampleLength;
-    SerializedParameterOverride m_OcclusionMinDistance;
-    SerializedParameterOverride m_OcclusionMaxDistance;
-    SerializedParameterOverride m_OcclusionBias;
-    SerializedParameterOverride m_OcclusionStrength;
-    SerializedParameterOverride m_OcclusionPower;
-    SerializedParameterOverride m_OcclusionColor;
-
     public override void OnEnable()
     {
         m_Blend = FindParameterOverride(x => x.Blend);
@@ -24,14 +16,6 @@ public sealed class ScreenSpaceReflectionEditor : PostProcessEffectEditor<Screen
         m_RayMaxDistance = FindParameterOverride(x => x.RayMaxDistance);
         m_ReflectionAdditionalRate = FindParameterOverride(x => x.ReflectionAdditionalRate);
         m_ReflectionRayThickness = FindParameterOverride(x => x.ReflectionRayThickness);
-        
-        m_OcclusionSampleLength = FindParameterOverride(x => x.OcclusionSampleLength);
-        m_OcclusionMinDistance = FindParameterOverride(x => x.OcclusionMinDistance);
-        m_OcclusionMaxDistance = FindParameterOverride(x => x.OcclusionMaxDistance);
-        m_OcclusionBias = FindParameterOverride(x => x.OcclusionBias);
-        m_OcclusionStrength = FindParameterOverride(x => x.OcclusionStrength);
-        m_OcclusionPower = FindParameterOverride(x => x.OcclusionPower);
-        m_OcclusionColor = FindParameterOverride(x => x.OcclusionColor);
     }
 
     public override void OnInspectorGUI()
@@ -41,13 +25,5 @@ public sealed class ScreenSpaceReflectionEditor : PostProcessEffectEditor<Screen
         PropertyField(m_RayMaxDistance);
         PropertyField(m_ReflectionAdditionalRate);
         PropertyField(m_ReflectionRayThickness);
-        
-        PropertyField(m_OcclusionSampleLength);
-        PropertyField(m_OcclusionMinDistance);
-        PropertyField(m_OcclusionMaxDistance);
-        PropertyField(m_OcclusionBias);
-        PropertyField(m_OcclusionStrength);
-        PropertyField(m_OcclusionPower);
-        PropertyField(m_OcclusionColor);
     }
 }
